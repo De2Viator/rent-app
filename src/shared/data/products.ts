8a6afec7646616923ae988c08be14e7df410000b
De2Viator <troshenkoyura@gmail.com>
2022-10-28 14:28:24 +0300

    fix filters

--- src/shared/data/products.ts
+++ src/shared/data/products.ts
@@ -2,7 +2,7 @@ import { Place } from "../types/place.js";
 import { SearchFormData } from "../types/search.js";
 
 export const search = async (searchData:SearchFormData, callback?:(results:Error|Place[]) => void): Promise<Error | Place[]> => {
-    const places = await fetch(`http://localhost:3030/places?coordinates=59.9386,30.3141&checkInDate=${searchData.checkIn.getTime()}&checkOutDate=${searchData.checkOut.getTime()}${searchData.price ? `&price=${searchData.price}` : ''}`)
+    const places = await fetch(`http://localhost:3030/places?coordinates=59.9386,30.3141&checkInDate=${searchData.checkIn.getTime()}&checkOutDate=${searchData.checkOut.getTime()}${searchData.price ? `&maxPrice=${searchData.price}` :''}`)
     .then(result => result.json());
     if(callback) {
         callback(places);
